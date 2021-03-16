232a83c6bc8c00a4a39cbc28838a7ee31b382296
ys杨硕(北京机票研发部) <shuo.yang@trip.com>
2021-03-16 10:05:03 +0800

    引入依赖

---
+++ src/main/java/com/yshuoo/anyutils/AnyUtilsApplication.java
@@ -0,0 +1,17 @@
+package com.yshuoo.anyutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yangshuo
+ * @date 2021/3/16 10:03
+ */
+@SpringBootApplication
+public class AnyUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnyUtilsApplication.class, args);
+    }
+
+}
